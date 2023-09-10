f3e6c4c26a126c862625d77e5aa3fff0aa98ade8
KabauPaatrick <94900211+KabauPaatrick@users.noreply.github.com>
2023-09-10 11:13:24 +0300

    Add files via upload
    Here are copies of my Utils data

---
+++ bitzweb/src/utils/TeamsData.js
@@ -0,0 +1,20 @@
+export const TeamsData = [
+  {
+    id: 0,
+    title: "Title1",
+    description: "CEO",
+    Image: "https://source.unsplash.com/random",
+  },
+  {
+    id: 1,
+    title: "Title2",
+    description: "Business Manager ",
+    Image: "https://source.unsplash.com/random",
+  },
+  {
+    id: 2,
+    title: "Title3",
+    description: " Developer ",
+    Image: "https://source.unsplash.com/random",
+  },
+];
