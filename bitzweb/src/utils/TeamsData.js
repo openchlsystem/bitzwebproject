db2237c3e7e19f7492e65b0b03c57a39ae23b48c
Patrick <kabaupaatrick@gmail.com>
2023-09-12 22:35:30 +0300

    Updated Json file and also modified the product view to render json file

--- bitzweb/src/utils/TeamsData.js
+++ bitzweb/src/utils/TeamsData.js
@@ -1,19 +1,19 @@
 export const TeamsData = [
   {
     id: 0,
-    title: "Title1",
+    title: "CEO",
     description: "CEO",
     Image: "https://source.unsplash.com/random",
   },
   {
     id: 1,
-    title: "Title2",
+    title: "COO",
     description: "Business Manager ",
     Image: "https://source.unsplash.com/random",
   },
   {
     id: 2,
-    title: "Title3",
+    title: "PROJECT MANAGER",
     description: " Developer ",
     Image: "https://source.unsplash.com/random",
   },
