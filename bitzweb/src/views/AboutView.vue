4604808b6cc8829b5fc948f67430f235ad1de7e5
Patrick <kabaupaatrick@gmail.com>
2023-09-13 15:30:43 +0300

    added clients at about us page

--- bitzweb/src/views/AboutView.vue
+++ bitzweb/src/views/AboutView.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  
+    <h1>Our  Clients</h1>
+    <div class="marquee-container">
+   <div  class="client -container">
+    <ul>
+
+      <li class="home" v-for="partner  in PartnersData" :key="partner.title">
+
+       <div class="row">
+        <div class="col-xs-4">
+        <img :src="partner.imageUrl" alt="">
+      <h3>{{ partner.title }}</h3>
+      <p>{{ partner .description }}</p>
+
+
+    </div>
+    </div>
+   </li>
+  </ul>
+
+   </div>
+
+
+
+   </div>
+    
+
+
+ 
 </template>
 
 <script>
+import { PartnersData } from '@/utils/Partners';
+
 export default {
-  name: "AboutView ",
-  components: {},
+setup(){
+  return{
+    PartnersData
+  }
+  
+}
+  
 };
 </script>
+<style>
+
+</style>
