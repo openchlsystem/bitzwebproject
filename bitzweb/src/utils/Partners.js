bcca9ca14a678e1ef7701af1d50f1f5eaba82a74
Patrick <kabaupaatrick@gmail.com>
2023-09-13 12:34:14 +0300

    added image logos for client, created json file called Partners.js

---
+++ bitzweb/src/utils/Partners.js
@@ -0,0 +1,87 @@
+import UnicefLogoImage from "../assets/images/stock/Unicef_logo.png";
+import UNFPALogoImage from "../assets/images/stock/UNFPA_logo.png";
+import TurkanaLogoImage from "../assets/images/stock/Turkana_Basin_Institutelogo.png";
+import TowerSaccoLogo from "../assets/images/stock/Tower_Sacco_Logo.png";
+import UnaitasLogo from "../assets/images/stock/Unaitas_Logo.png";
+import RwandaLogo from "../assets/images/stock/Rwanda_Biomedical_Center_Logo.png";
+import SautiLogo from "../assets/images/stock/Sauti_Logo.png";
+import ChildlineLogo from "../assets/images/stock/ChildlineKenya.png";
+import Eprocurement from "../assets/images/stock/Rwanda_Eprocurement.jpeg";
+import JabaliSchoolLogo from "../assets/images/stock/Jabali_Christian_School_logo.png"
+export const PartnersData = [
+    {
+        id: 0,
+        title:"Unicef For Every Child",
+        description:"Built the child helpline Case Management and Call Center for Childline Kenya. The work done was, Requirement,Gathering, wireframing, SoftwareDevelopment, Deployment, user testing, Training, and User Support.Project Timelines was 8 Months",
+        imageUrl:UnicefLogoImage,
+        Link:"https://www.unicef.org/child-alert/severe-wasting"
+
+    },
+    {
+        id:1,
+        title:"UNFPA",
+        description:"Built the child helpline Case Management and Call Center for Childline Kenya. The work done was, Requirement,Gathering, wireframing, SoftwareDevelopment, Deployment, user testing, Training, and User Support.Project Timelines was 8 Months",
+        imageUrl:UNFPALogoImage,
+        Link:"https://www.unfpa.org/",
+
+    },
+    {
+        id:2,
+        title: "Turkana Basin Institute",
+        description:"",
+        imageUrl:TurkanaLogoImage,
+        Link:"https://www.turkanabasin.org/"
+    },
+    {
+        id:3,
+        title: "Sauti 116 Helpline",
+        description:"",
+        imageUrl:SautiLogo,
+        Link:"https://sauti.mglsd.go.ug/sauti/"
+    },
+    {
+        id:4,
+        title: "Tower Sacco",
+        description:"",
+        imageUrl:TowerSaccoLogo,
+        Link:"https://towersacco.co.ke/"
+
+    },
+    {
+        id:5,
+        title:" Unaitas",
+        description:"",
+        imageUrl:UnaitasLogo,
+        Link:"https://unaitas.com/",
+    },
+    {
+        id:6,
+        title:"Rwanda Biomedical Center",
+        description:"",
+        imageUrl:RwandaLogo,
+        Link:"https://www.rbc.gov.rw/index.php?id=188",
+    },
+    {
+        id:7,
+        title:"Childline Kenya",
+        description:"",
+        imageUrl:ChildlineLogo,
+        Link:"https://childlinekenya.co.ke/"
+    },
+    {
+        id:8,
+        title:"Rwanda Online Eprocurement System",
+        description:"",
+        imageUrl:Eprocurement,
+        Link:"https://www.umucyo.gov.rw/",
+    },
+    {
+        id:9,
+        title:"Jabali Christian School",
+        description:"",
+        imageUrl:JabaliSchoolLogo,
+        Link:"https://jabali.ac.ke/index.php/en/"
+    }
+
+   
+]
