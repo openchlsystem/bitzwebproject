e934ec1550834f7eab1cfbe08bd79ee734183dbf
Patrick <kabaupaatrick@gmail.com>
2023-09-19 11:57:11 +0300

    routed the product hero section successfully

---
+++ bitzweb/src/utils/SurveyData.js
@@ -0,0 +1,11 @@
+export const surveyJson={
+    elements: [{
+      name:"First Name",
+        title:"Enter your first name:" ,
+        type:"text"
+    }, {
+      name:"Last Name",
+        title:"Enter your last name:" ,
+        type:"text"
+    }]
+}
