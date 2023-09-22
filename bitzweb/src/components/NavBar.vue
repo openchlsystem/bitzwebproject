90e6d1be13f378ac51b6136e068f21a445f47186
Nelson Adagi <thinkiqbs@gmail.com>
2023-09-22 18:13:46 +0300

    fixed css for footer

--- bitzweb/src/components/NavBar.vue
+++ bitzweb/src/components/NavBar.vue
@@ -1,49 +1,50 @@
 <template>
-  <nav class="navbar">
+  <div class="menu-section">
     <div class="logo">
       <a href="/"><img src="@/assets/logo.png" alt="Logo" /></a>
     </div>
-    <div class="dropdown">
-      <router-link to="/">Home</router-link>
-    </div>
 
-    <div class="dropdown">
-      <router-link to="/products">Product</router-link>
-      <div class="dropdown-content">
-        <ul>
-          <li
-            v-for="product in productsData"
-            :key="product.id"
-            @click="this.$router.push(`/products/${product.id}`)"
-          >
-            <p>{{ product.menu_name }}</p>
-            <span>{{ product.menu_description }}</span>
-          </li>
-        </ul>
+    <nav class="navbar">
+
+      <div class="dropdown">
+        <router-link to="/">Home</router-link>
       </div>
 
-      <!--- </div>
-    <div class="dropdown">
-     <router-link to="/solutions">Solutions</router-link>
-      <div class="dropdown-content">
-        <router-link to="/solutions/item1">Item 1</router-link>
-        <router-link to="/solutions/item2">Item 2</router-link>
-        <router-link to="/solutions/item3">Item 3</router-link>
-      </div>-->
-    </div>
-    <div class="dropdown">
-      <router-link to="/developers">Developers</router-link>
-    </div>
-    <div class="dropdown">
-      <router-link to="/pricing">Pricing</router-link>
-    </div>
-    <div class="dropdown">
-      <router-link to="/blog">Blog</router-link>
-    </div>
-    <div class="dropdown">
-      <router-link to="/about">About</router-link>
-    </div>
-  </nav>
+      <div class="dropdown">
+        <router-link to="/products">Product</router-link>
+        <div class="dropdown-content">
+          <ul>
+            <li v-for="product in productsData" :key="product.id" @click="this.$router.push(`/products/${product.id}`)">
+              <p>{{ product.menu_name }}</p>
+              <span>{{ product.menu_description }}</span>
+            </li>
+          </ul>
+        </div>
+
+
+      </div>
+      <div class="dropdown">
+        <router-link to="/developers">Developers</router-link>
+      </div>
+      <div class="dropdown">
+        <router-link to="/pricing">Get Quote</router-link>
+      </div>
+      
+      
+    </nav>
+    <nav>
+      <div class="dropdown">
+        <router-link to="/contact">Contact</router-link>
+      </div>
+      <div class="dropdwon">
+        <router-link to="/about">company</router-link>
+      </div>
+      <div class="dropdown">
+          <router-link to="/blog">Blog</router-link>
+        </div>
+    </nav>
+
+  </div>
 </template>
 
 <script>
