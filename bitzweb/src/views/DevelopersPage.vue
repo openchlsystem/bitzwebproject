0781d00b806030dec3f18a7a9cb931b7f3db1db7
Patrick <kabaupaatrick@gmail.com>
2023-09-26 12:24:50 +0300

    replaced images and also created a developer questionnaire for the career page

--- bitzweb/src/views/DevelopersPage.vue
+++ bitzweb/src/views/DevelopersPage.vue
@@ -1,11 +1,39 @@
 <template>
-  <h2>Welcome to the developers page</h2>
+  <div id="development">
+    <h2>Development Team</h2>
+    <h3>Responsibilities:</h3>
+    <ul>
+      <li>Collaboratively planning and estimating work</li>
+      <li>Developing, testing, and delivering high-quality product increments</li>
+      <li>Collaborating with the Product Owner</li>
+      <li>Embracing self-organization and continuous improvement</li>
+    </ul>
+    <h3>Requirements:</h3>
+    <ul>
+      <li>Technical expertise and skills relevant to the project</li>
+      <li>Proficiency in relevant technologies, tools, and frameworks</li>
+      <li>Collaborative mindset and ability to work well in a team</li>
+      <li>Self-organization and time management abilities</li>
+      <li>Quality-focused approach and attention to detail</li>
+      <li>Continuous learning and improvement mindset</li>
+      <li>Effective communication and active listening skills</li>
+      <li>Adaptability and flexibility</li>
+    </ul>
+    <button @click="apply">Apply</button>
+  </div>
 </template>
 
 <script>
 export default {
-  setup() {},
+  methods: {
+    apply() {
+      if (this.$route.path === "/developers") {
+        this.$router.push("/careers");
+       }
+      // Handle apply logic
+      console.log('Applied for Development Team role.');
+    }
+  }
+ 
 };
 </script>
-
-<style lang="scss" scoped></style>
